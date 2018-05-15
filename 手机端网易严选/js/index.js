d8712fccfc7bdf63d4c7c73f445aee40b5272617
Ben <13726683513@163.com>
2018-05-15 22:23:10 +0800

    commit

--- 手机端网易严选/js/index.js
+++ 手机端网易严选/js/index.js
@@ -1,4 +1,13 @@
 (function(){
+    var starserch=$(".y-slide").html();  //获取初始的搜索内容
+    $(".y-slide").click(function(){
+        $(".y-slide").empty();  //获取焦点清空搜索
+       
+    });
+    $(".y-slide").blur(function(){
+        $(".y-slide").html(starserch);  //失去焦点拿回搜索
+    });
+    
     var nav=$('.nav span');
     nav[0].className='redline';
     for(i=0;i<nav.length;i++){
@@ -60,4 +69,47 @@
     })
     function getStyle(ele){return ele.currentStyle || getComputedStyle(ele);}
     //获取样式兼容
+    var startTme=new Date();
+    show();  //限购时间
+    function show(){
+        var startTime=new Date();
+        var ed=startTme;
+        ed.setHours(14);
+        ed.setMinutes(00);
+        ed.setSeconds(00);
+        ed.setMilliseconds(00);
+        var leaveTime=parseInt((ed.getTime()-startTime.getTime())/1000);
+        var d=parseInt(leaveTime/(24*60*60));
+        var h=parseInt(leaveTime/(60*60)%24);
+        var m=parseInt(leaveTime/60%60);
+        var s=parseInt(leaveTime%60);
+        m=cleartime(m);
+        h=cleartime(h);
+        s=cleartime(s);
+        if (leaveTime>0&&ed.getTime()===startTime.getTime()) {
+            document.querySelector(".time-left a").innerHTML="<h3>严选限时购</h3><span>"+h+"</span><span>:</span><span>"+m+"</span><span>:</span><span>"+s+"</span><p>下一场 14:00 开始</p>";
+        }else if(leaveTime<=0||ed.getTime()>startTime.getTime()){    //判断倒计时是否结束
+            ed.setDate(ed.getDate()+1);
+            ed.setHours(02);
+            ed.setMinutes(00);
+            ed.setSeconds(00);
+            ed.setMilliseconds(00);
+            leaveTime=parseInt((ed.getTime()-startTime.getTime())/1000);
+            d=parseInt(leaveTime/(24*60*60));
+            h=parseInt(leaveTime/(60*60)%24);
+            m=parseInt(leaveTime/60%60);
+            s=parseInt(leaveTime%60);
+            m=cleartime(m);
+            h=cleartime(h);
+            s=cleartime(s);
+            document.querySelector(".time-left a").innerHTML="<h3>团购结束</h3><span>"+h+"</span><span>:</span><span>"+m+"</span><span>:</span><span>"+s+"</span><p>下一场 明天14:00 开始</p>";
+        };
+        setTimeout(show,1000);
+    }
+    function cleartime(i){
+        if (i<=10) {
+            i="0"+i
+        }
+        return i
+    }
 })();
